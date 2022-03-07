cfddb9f04330c43b911b05f64482e19bf73e66e0
Zehra Karaman <zehrakaramaan@gmail.com>
2022-03-08 00:27:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Login from "./components/Login";
 import Map from "./components/Map";
 
 function App() {
   return (
-    <Login />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/map" element={<Map/>} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
