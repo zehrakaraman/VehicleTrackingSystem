90b2e5d570351a1f3b1e4477d4dfc969222f982a
Zehra Karaman <zehrakaramaan@gmail.com>
2022-03-13 01:32:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Login from "./components/Login";
 import Map from "./components/Map";
+import GridTable from "./components/GridTable"
+import Dashboard from "./components/Dashboard"
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Login/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/map" element={<Map/>} />
+        <Route path="/grid" element={<GridTable/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
     </Router>
   );
